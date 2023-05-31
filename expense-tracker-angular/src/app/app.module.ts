2660fbfca9b7ab85d0f84c7084d82bd41a621cd7
justsaykk <kaikein.woo@gmail.com>
2023-05-31 14:52:36 +0800

    added angular materials and firebase authentication

--- expense-tracker-angular/src/app/app.module.ts
+++ expense-tracker-angular/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ expense-tracker-angular/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'expense-tracker-ff7a9',
+    appId: '1:866744252192:web:0ac120f92d9e004aa58945',
+    storageBucket: 'expense-tracker-ff7a9.appspot.com',
+    apiKey: 'AIzaSyDqy38QhMI6Y38accLyOEghXOvkbB4q7N8',
+    authDomain: 'expense-tracker-ff7a9.firebaseapp.com',
+    messagingSenderId: '866744252192',
+    measurementId: 'G-TLRWQFPSV4',
+  },
+};
