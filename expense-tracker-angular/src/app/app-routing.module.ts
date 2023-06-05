13433365d3ee7351b8e6ee0905bc516aaf015d85
justsaykk <kaikein.woo@gmail.com>
2023-06-05 13:52:31 +0800

    Created initial dashboard layout

--- expense-tracker-angular/src/app/app-routing.module.ts
+++ expense-tracker-angular/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { SignUpComponent } from './views/sign-up/sign-up.component';
+import { DashComponent } from './views/dash/dash.component';
 
 const routes: Routes = [
+  {path: "dashboard", component: DashComponent},
   {path: "login", component: LoginComponent},
   {path: "signup", component: SignUpComponent},
   // Catch-all
